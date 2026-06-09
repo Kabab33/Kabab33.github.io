ba71f3fc510ad795b5fc70f625a63213f3b27379
Kabab33 <kabab33@kabab33.nl>
2026-06-09 19:57:37 +0200

    her commit alles ig?
    git doet weer eens raar. whatever nieuwe branch voor nog een redisign
    (ik geloof dat dat verkeerd gespeld is)

---
+++ assets/img/bg/README.md
@@ -0,0 +1,1 @@
+From https://goblin-heart.net/sadgrl/webmastery/downloads/tiledbgs
--- data/js/utils.js
+++
@@ -1,6 +0,0 @@
-function redirect(url) {
-    window.location.href = url; 
-};
-function chance(oneIn) {
-    return Math.floor(Math.random() * oneIn) == 0;
-};
